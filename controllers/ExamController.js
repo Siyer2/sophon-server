a3e982738d09655c13c28de99e5b21a1205f4ab9
Syam Iyer <iyersyam@gmail.com>
2020-01-19 15:40:46 +1100

    Restructured file

--- controllers/ExamController.js
+++ controllers/ExamController.js
@@ -15,6 +15,43 @@ const dummyData = {
     }
 }
 
+router.post('/test', async function (request, response) {
+    try {
+        const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+        let _container;
+
+        const promisifyStream = stream => new Promise((resolve, reject) => {
+            stream.on('data', data => console.log(data.toString()))
+            stream.on('end', resolve)
+            stream.on('error', reject)
+        });
+
+        docker.container.create({
+            Image: 'kaixhin/vnc', 
+            PublishAllPorts: true, 
+            name: 'test'
+        })
+            .then(container => container.start())
+            .then(container => {
+                _container = container;
+                return container.exec.create({
+                    AttachStdout: true,
+                    AttachStderr: true,
+                    Cmd: ['apt-get', 'install', 'libreoffice', '-y']
+                })
+            })
+            .then(exec => {
+                return exec.start({ Detach: false })
+            })
+            .then(stream => promisifyStream(stream))
+            // .then(() => _container.kill())
+            .catch(error => console.log(error));
+        return response.json({ "done": "done" });
+    } catch (error) {
+        return response.status(500).json({ error });
+    }
+})
+
 // Student enters an exam
 router.post('/enter', async function (request, response) {
     try {
@@ -26,20 +63,12 @@ router.post('/enter', async function (request, response) {
             PublishAllPorts: true
         });
         await createdContainer.start();
-        const port = (await createdContainer.status);
-        console.log("port", port);
 
         // Add the required applications to the OS
-        const applicationResult = await createdContainer.exec.create({
-            AttachStdout: true,
-            AttachStderr: true,
-            Cmd: [
-                'apt-get update', 
-                'apt-get install libreoffice -y'
-            ]
-        });
-        console.log("appRes", applicationResult);
-        await applicationResult.start();
+        await runCommand(createdContainer, ['apt-get', 'install']);
+        console.log("end command 1");
+        await runCommand(createdContainer, ['apt-get', 'install', 'libreoffice', '-y', '--fix-missing']);
+        console.log("end command 2");
 
         // Run the proxy server
         //websockify({ source: 'localhost:5901', target: 'localhost:32770' });
@@ -58,6 +87,42 @@ function createAndRunContainer(docker) {
             
         } catch (ex) {
             console.log("ERROR CREATING AND RUNNING CONTAINER");
+            reject(ex);
+        }
+    });
+}
+
+// Run command on container
+function runCommand(container, command) {
+    console.log("beginning command", command);
+    const promisifyStream = stream => new Promise((resolve, reject) => {
+        stream.on('data', data => console.log(data.toString()))
+        stream.on('end', resolve)
+        stream.on('error', reject)
+    });
+
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            container.exec.create({
+                AttachStdout: true,
+                AttachStderr: true,
+                Cmd: command, 
+                Tty: true
+            })
+            .then(exec => {
+                return exec.start({ Detach: false });
+            })
+            .then(async stream => {
+                const results = await promisifyStream(stream);
+                resolve(results);
+            })
+            .catch(error => {
+                console.log("ERROR RUNNING COMMAND", error);
+                reject(ex);
+            });
+        } catch (ex) {
+            console.log("ERROR RUNNING COMMAND", ex);
             reject(ex);
         }
     });
