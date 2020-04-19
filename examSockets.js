ee025f0a4654f875e7753e1295622e26af816875
Syam Iyer <iyersyam@gmail.com>
2020-04-19 14:48:01 +1000

    Removed the infos emit

--- examSockets.js
+++ examSockets.js
@@ -7,46 +7,48 @@ module.exports = function (server) {
     var rdpClient = null;
     
     io.on('connection', async function (client) {
-        client.on('infos', function (infos) {
-            const domain = `ec2-${infos.ip.replace('.', '-')}.ap-southeast-2.compute.amazonaws.com`;
-            const connection = {
-                domain: domain, 
-                username: "DefaultAccount", 
-                password: "4mbA49H?vdO-mIp(=nTeP*psl4*j=Vwt", 
-                ip: infos.ip, 
-                port: "3389", 
-                screen: { width: 800, height: 600 }, 
-                locale: "en"
-            }
+        const ip = client.handshake.query['ip'];
+        console.log("ip", ip);
+        const domain = `ec2-${ip.replace('.', '-')}.ap-southeast-2.compute.amazonaws.com`;
+        const connection = {
+            domain: domain, 
+            username: "DefaultAccount", 
+            password: "4mbA49H?vdO-mIp(=nTeP*psl4*j=Vwt", 
+            ip: ip, 
+            port: 3389, 
+            screen: { width: 800, height: 600 }, 
+            locale: "en"
+        }
 
-            if (rdpClient) {
-                // clean older connection
-                rdpClient.close();
-            };
+        if (rdpClient) {
+            // clean older connection
+            rdpClient.close();
+        };
 
-            rdpClient = rdp.createClient({
-                domain: connection.domain,
-                userName: connection.username,
-                password: connection.password,
-                enablePerf: true,
-                autoLogin: true,
-                screen: connection.screen,
-                locale: connection.locale,
-                logLevel: process.argv[2] || 'INFO'
-            }).on('connect', function () {
-                // Enter the exam
-                console.log("connected");
-                client.emit('rdp-connect');
-            }).on('bitmap', function (bitmap) {
-                client.emit('rdp-bitmap', bitmap);
-            }).on('close', function () {
-                // Submit the exam
-                
-                client.emit('rdp-close');
-            }).on('error', function (err) {
-                client.emit('rdp-error', err);
-            }).connect(connection.ip, connection.port);
-        }).on('mouse', function (x, y, button, isPressed) {
+        rdpClient = rdp.createClient({
+            domain: connection.domain,
+            userName: connection.username,
+            password: connection.password,
+            enablePerf: true,
+            autoLogin: true,
+            screen: connection.screen,
+            locale: connection.locale,
+            logLevel: process.argv[2] || 'INFO'
+        }).on('connect', function () {
+            // Enter the exam
+            console.log("connected");
+            client.emit('rdp-connect');
+        }).on('bitmap', function (bitmap) {
+            client.emit('rdp-bitmap', bitmap);
+        }).on('close', function () {
+            // Submit the exam
+            
+            client.emit('rdp-close');
+        }).on('error', function (err) {
+            client.emit('rdp-error', err);
+        }).connect(connection.ip, connection.port);
+
+        client.on('mouse', function (x, y, button, isPressed) {
             if (!rdpClient) return;
 
             rdpClient.sendPointerEvent(x, y, button, isPressed);
