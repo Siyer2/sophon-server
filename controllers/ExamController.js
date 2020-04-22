7a467a5d7eda998bb87dbf68b1a7edc48d12b2c5
Syam Iyer <iyersyam@gmail.com>
2020-04-23 09:38:43 +1000

    Reverted to entrace id not ip address return

--- controllers/ExamController.js
+++ controllers/ExamController.js
@@ -100,7 +100,7 @@ router.post('/enter', async function (request, response) {
         console.log("Finished pushing all files");
 
         // Store the student entrance in Mongo
-        await request.db.collection("examEntrances").insertOne({
+        const examEntrance = await request.db.collection("examEntrances").insertOne({
             ip: targetHost,
             examId: String(exam._id),
             studentId,
@@ -108,7 +108,7 @@ router.post('/enter', async function (request, response) {
             startTime: moment().utc().format()
         });
 
-        return response.json({ status: 'ready', ip: targetHost });
+        return response.json({ status: 'ready', examEntranceId: examEntrance.ops[0]._id.toString() });
     } catch (error) {
         return response.status(500).json({ error });
     }
